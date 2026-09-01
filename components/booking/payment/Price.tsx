bb0b8467eb029288e9436609ae189e7134698da4
Fateme Mashayekhi <mashayekhimaral@gmail.com>
2026-09-01 19:36:46 +0330

    fix : fixed payment page logic

--- components/booking/payment/Price.tsx
+++ components/booking/payment/Price.tsx
@@ -1,34 +1,24 @@
 "use client";
-import { getAppointmentInfo } from "@/api/services";
 import { faPrice } from "@/lib/utils";
 import { useBookingStore } from "@/stores/bookingStore";
-import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Price() {
   const router = useRouter();
-  const appointment_id = useBookingStore((state) => state.appointmentId);
+  const { service, appointmentId } = useBookingStore();
 
   useEffect(() => {
-    if (!appointment_id) {
+    if (!appointmentId) {
       router.replace("/booking");
     }
-  }, [appointment_id, router]);
-
-  const { data } = useQuery({
-    queryKey: ["appointment-information", appointment_id],
-    queryFn: () => getAppointmentInfo(appointment_id ?? 1),
-    enabled: Boolean(appointment_id),
-    staleTime: 0,
-    refetchOnMount: "always",
-  });
+  }, [appointmentId, router]);
   return (
     <div className="border border-[#F68FA4] bg-pink-50 rounded-xl p-4 flex flex-col justify-center items-center gap-y-4">
       <span>مبلغ قابل پرداخت</span>
       <div className="flex items-center gap-x-1.5">
         <span className="font-semibold text-3xl">
-          {data?.reserve_fee && faPrice(data.reserve_fee)}
+          {faPrice(service?.price ? service?.price : "")}
         </span>
         <span>تومان</span>
       </div>
