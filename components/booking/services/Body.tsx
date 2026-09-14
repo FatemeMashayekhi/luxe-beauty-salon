fd9cc30df595cc4483b6b2edc6b3eee0c56c1bb4
Fateme Mashayekhi <mashayekhimaral@gmail.com>
2026-09-14 13:34:45 +0330

    style : changed services card design

--- components/booking/services/Body.tsx
+++ components/booking/services/Body.tsx
@@ -26,10 +26,21 @@ export default function Body() {
   return (
     <div className="p-4 flex flex-col gap-y-8 lg:w-[70%]">
       <SearchInput search={search} setSearch={setSearch} />
-      <CategoriesList
-        selectedCategory={selectedCategory}
-        setSelectedCategory={setSelectedCategory}
-      />
+
+      <div className="flex flex-col gap-y-3">
+        <h2 className="text-base font-semibold text-[#292929]">
+          دسته‌بندی خدمات
+        </h2>
+
+        <p className="text-sm text-[#999]">
+          دسته‌بندی مورد نظر خود را انتخاب کنید
+        </p>
+        <CategoriesList
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+      </div>
+
       <ServicesList
         handleSelectService={handleSelectService}
         categoryId={selectedCategory}
