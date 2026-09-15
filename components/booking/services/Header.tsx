9f91b17df5dd50e181bc09bdb31ae6d65df4e9a8
Fateme Mashayekhi <mashayekhimaral@gmail.com>
2026-09-15 19:21:49 +0330

    fix : added path to services header

--- components/booking/services/Header.tsx
+++ components/booking/services/Header.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import logo from "@/public/images/logo/logo.png";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 interface CurrentPath {
   [key: string]: string;
@@ -17,6 +18,7 @@ export default function Header() {
     "/booking/payment": "پرداخت نهایی",
     "/booking/review": "بررسی اطلاعات رزرو",
     "/booking/schedule": "انتخاب زمان",
+    "/profile": "پروفایل کاربری",
   };
   const pageTitle = currentPath[pathName] ? currentPath[pathName] : "";
 
@@ -24,9 +26,15 @@ export default function Header() {
     <header className="flex justify-between items-center p-4 border-b border-[#E8E8E8] lg:p-6">
       <span className="font-semibold">{pageTitle}</span>
       <menu className="hidden lg:flex lg:gap-x-8 lg:text-lg">
-        <li>خانه</li>
-        <li>پروفایل</li>
-        <li>خدمات</li>
+        <li>
+          <Link href={"/"}>خانه</Link>
+        </li>
+        <li>
+          <Link href={"/profile"}>پروفایل</Link>
+        </li>
+        <li>
+          <Link href={"/booking"}>خدمات</Link>
+        </li>
         {/* <li>فروشگاه</li> */}
       </menu>
       <Image src={logo} alt="logo" width={140} />
